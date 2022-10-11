641070d363b28d3f7899778b318f42ae62274883
ScLuth12 <114996392+ScLuth12@users.noreply.github.com>
2022-10-11 11:22:10 +0700

    tah sepp
    tah sepp

--- Latihan2.java
+++ Latihan2.java
@@ -13,6 +13,10 @@ public class Latihan2 extends javax.swing.JFrame {
     /**
      * Creates new form Latihan2
      */
+    public String KdBis = "";
+    public String FltBis = "";
+    public long HrgBis;
+    
     public Latihan2() {
         initComponents();
     }
@@ -26,21 +30,163 @@ public class Latihan2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jToggleButton1 = new javax.swing.JToggleButton();
+        jLabel1 = new javax.swing.JLabel();
+        radio1 = new javax.swing.JRadioButton();
+        TxtKodebis = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        radio2 = new javax.swing.JRadioButton();
+        radio3 = new javax.swing.JRadioButton();
+        TxtHarga = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+
+        jToggleButton1.setText("jToggleButton1");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Kode Bis");
+
+        radio1.setText("SUPER EKSEKUTIF");
+        radio1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                radio1ActionPerformed(evt);
+            }
+        });
+
+        TxtKodebis.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TxtKodebisActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Fasilitas");
+
+        radio2.setText("EKSEKUTIF");
+        radio2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                radio2ActionPerformed(evt);
+            }
+        });
+
+        radio3.setText("BISNIS AC");
+        radio3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                radio3ActionPerformed(evt);
+            }
+        });
+
+        TxtHarga.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TxtHargaActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Harga");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(75, 75, 75)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addGap(39, 39, 39)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(radio3)
+                    .addComponent(radio1)
+                    .addComponent(TxtKodebis)
+                    .addComponent(radio2)
+                    .addComponent(TxtHarga, javax.swing.GroupLayout.DEFAULT_SIZE, 122, Short.MAX_VALUE))
+                .addContainerGap(124, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TxtKodebis, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(radio1)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(radio2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(radio3)
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(TxtHarga, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addContainerGap(62, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void TxtKodebisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtKodebisActionPerformed
+        // TODO add your handling code here:
+        KdBis = TxtKodebis.getText();
+       if(KdBis.equals("BIS001") || KdBis.equals("bis001"))
+       {
+           //HrgBis = 550000;
+       }
+       else if(KdBis.equals("BIS002") || KdBis.equals("bis002"))
+       {
+           //HrgBis = 450000;
+       }
+       else if(KdBis.equals("BIS003") || KdBis.equals("bis003"))
+       {
+           //HrgBis = 250000;
+       }
+    }//GEN-LAST:event_TxtKodebisActionPerformed
+
+    private void TxtHargaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtHargaActionPerformed
+        // TODO add your handling code here:
+        TxtHarga.setText(String.valueOf(HrgBis));
+    }//GEN-LAST:event_TxtHargaActionPerformed
+
+    private void radio1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radio1ActionPerformed
+        // TODO add your handling code here:
+        if(radio1.isSelected()==true&&radio2.isSelected()==false&&radio3.isSelected()==false ||
+           radio1.isSelected()==false&&radio2.isSelected()==true &&radio3.isSelected()==false) {
+                TxtHarga.setText("Rp.550.000");
+                TxtKodebis.setText("BIS001");
+        }
+        else {
+            TxtKodebis.setText("");
+            TxtHarga.setText("Rp. 000.000");
+        }
+    }//GEN-LAST:event_radio1ActionPerformed
+
+    private void radio2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radio2ActionPerformed
+        // TODO add your handling code here:
+        if(radio1.isSelected()==false&&radio2.isSelected()==true&&radio3.isSelected()==false ||
+           radio1.isSelected()==false&&radio2.isSelected()==true &&radio3.isSelected()==false) {
+                TxtHarga.setText("Rp.450.000");
+                TxtKodebis.setText("BIS002");
+        }
+        else {
+            TxtKodebis.setText("");
+            TxtHarga.setText("Rp. 000.000");
+        }
+    }//GEN-LAST:event_radio2ActionPerformed
+
+    private void radio3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radio3ActionPerformed
+        // TODO add your handling code here:
+        if(radio1.isSelected()==false&&radio2.isSelected()==false&&radio3.isSelected()==true ||
+           radio1.isSelected()==false&&radio2.isSelected()==false &&radio3.isSelected()==true) {
+                TxtHarga.setText("Rp.250.000");
+                TxtKodebis.setText("BIS003");
+        }
+        else {
+            TxtKodebis.setText("");
+            TxtHarga.setText("Rp. 000.000");
+        }
+    }//GEN-LAST:event_radio3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +224,14 @@ public class Latihan2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField TxtHarga;
+    private javax.swing.JTextField TxtKodebis;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JRadioButton radio1;
+    private javax.swing.JRadioButton radio2;
+    private javax.swing.JRadioButton radio3;
     // End of variables declaration//GEN-END:variables
 }
